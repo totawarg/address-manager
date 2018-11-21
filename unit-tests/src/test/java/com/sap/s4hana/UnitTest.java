0f588c16b40f1405e9bc387feb95cd417079a6fa
Goverdhan Totawar <totawarg@gmail.com>
2018-11-21 18:33:50 +0200

    first commit

---
+++ unit-tests/src/test/java/com/sap/s4hana/UnitTest.java
@@ -0,0 +1,12 @@
+package com.sap.s4hana;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
